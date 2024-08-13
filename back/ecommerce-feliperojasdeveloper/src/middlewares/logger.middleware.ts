2fdf6d9a06447805b1c2c58f96fa958495c07659
Felipe Rojas <feliperojasestudia@gmail.com>
2024-08-12 21:38:22 -0500

    Organice middleware to show date completly

--- back/ecommerce-feliperojasdeveloper/src/middlewares/logger.middleware.ts
+++ back/ecommerce-feliperojasdeveloper/src/middlewares/logger.middleware.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 
 export function loggerGlobal(req: Request, res: Response, next: NextFunction){
-    const currentTime = new Date().toLocaleTimeString();
+    const currentTime = new Date();
     console.log(`Estas ejecutando un método ${req.method} en la ruta ${req.url} a las ${currentTime}`);
     next();
 }
